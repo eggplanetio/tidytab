4c11f58ce72b2f707c285dbff85556f657f5b2d7
Brian Gonzalez <me@briangonzalez.org>
2017-03-27 07:16:50 -0700

    dashboard stats

---
+++ app/component/dashboard-stats.vue
@@ -0,0 +1,22 @@
+<template>
+  <span class="stats">
+    23 windows tidied
+  </span>
+</template>
+
+<script>
+export default {
+  props: [
+    'tabGroups'
+  ]
+}
+</script>
+
+<style lang="sass" scoped="true">
+@import "../styles/settings";
+
+.stats {
+  color: $color-secondary;
+  font-weight: 300;
+}
+</style>
