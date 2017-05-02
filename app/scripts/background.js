361c52f6fc549a610604025b985e7b26561e26df
Brian Gonzalez <me@briangonzalez.org>
2017-05-02 13:47:04 +0900

    Fixes issue where dashboard sometimes does not open because popup has closed; this commit makes the background page responsible for all closing/dashboard showing

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -5,12 +5,27 @@ import 'babel-polyfill';
 import ChromePromise from 'chrome-promise';
 const chromep = new ChromePromise()
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
+const viewDashboard = async () => {
+  const currentWindow = await chromep.windows.getCurrent({});
+  const tabs = await chromep.tabs.getAllInWindow(currentWindow.id);
+  const isShowingDashboard = tabs[0].title === "TidyTab – Dashboard";
+  const url = chrome.extension.getURL('pages/dashboard.html');
+  if (!isShowingDashboard) {
+    await chromep.tabs.create({ url, pinned: true, index: 0 });
+  }
+  chrome.tabs.highlight({ tabs: [0] });
+};
+
+chrome.runtime.onMessage.addListener(async event => {
+  console.log('Message received!', event);
+
+  if (event.message === 'viewDashboard') {
+    await viewDashboard();
+  }
+
+  if (event.message === 'removeTabs') {
+    if (event.data.viewDashboard) await viewDashboard();
+    await chromep.tabs.remove(event.data.tabIds);
+  }
 });
 
-chrome.browserAction.setBadgeText({text: ''});
-
-chrome.commands.onCommand.addListener(function(command) {
-  console.log('Command:', command);
-});
