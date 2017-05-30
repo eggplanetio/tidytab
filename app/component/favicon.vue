d692597d26d704e5c07930b1af84823fd550a858
Brian Gonzalez <me@briangonzalez.org>
2017-05-30 08:27:38 -0700

    WIP: Async loading of favicons (Issue #21)

--- app/component/favicon.vue
+++ app/component/favicon.vue
@@ -1,20 +1,56 @@
 <template>
   <span>
-    <img :src="faviconUrl" :title="title">
+    <img v-if="faviconUrl" :src="faviconUrl" :title="title">
   </span>
 </template>
 
 <script>
+function loadImgAsDataUri (url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest()
+    xhr.open('GET', url, true)
+    xhr.responseType = 'arraybuffer'
+
+    xhr.onerror = () => resolve()
+    xhr.onload = () => {
+      const arrayBufferView = new Uint8Array(xhr.response)
+      const blob = new Blob([ arrayBufferView ], { type: 'image/jpeg' })
+      const reader = new window.FileReader()
+      reader.onloadend = () => {
+        if (reader.result === 'data:') return resolve()
+        resolve(reader.result)
+      }
+      reader.readAsDataURL(blob)
+    }
+
+    xhr.send()
+  })
+}
+
 export default {
   props: [
     'url',
     'title'
   ],
-  computed: {
-    faviconUrl () {
-      const parser = document.createElement('a')
-      parser.href = this.url
-      return `https://www.google.com/s2/favicons?domain=${parser.host}`
+  data () {
+    return { faviconUrl: false }
+  },
+  async created () {
+    const parser = document.createElement('a')
+    parser.href = this.url
+
+    const urls = [
+      `${parser.protocol}//${parser.host}/favicon.ico`,            // Favicon from site
+      `https://www.google.com/s2/favicons?domain=${parser.host}`,   // Favicon from Google
+      `generic-favicon`
+    ]
+
+    let blobUrl = await loadImgAsDataUri(urls[0])
+    if (blobUrl) {
+      this.faviconUrl = blobUrl
+    } else {
+      blobUrl = await loadImgAsDataUri(urls[1])
+      this.faviconUrl = blobUrl
     }
   }
 }
