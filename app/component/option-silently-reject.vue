6d3845c9ff5396a3914b2dfc9769302b495f616d
Brian Gonzalez <me@briangonzalez.org>
2017-06-03 07:04:51 -0700

    [Option] Silently reject dups (Fixes #49)

---
+++ app/component/option-silently-reject.vue
@@ -0,0 +1,28 @@
+<template>
+  <span>
+    <label for="silently-reject">
+      <input
+        type="checkbox"
+        name="silently-reject"
+        v-model="silentlyRejectDuplicates"
+        @change="onSelect"> Silently reject duplicates
+    </label>
+  </span>
+</template>
+
+<script>
+import store from '../store/index.js'
+import { mapState } from 'vuex'
+
+export default {
+  methods: {
+    onSelect (e) {
+      store.commit('SET_SILENTLY_REJECT_DUPLICATES', e.target.checked)
+    }
+  },
+
+  computed: {
+    ...mapState([ 'silentlyRejectDuplicates' ])
+  }
+}
+</script>
